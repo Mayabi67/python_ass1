acf3069bff1107dd9fbea1ab701becfdd55b0215
mayabi67 <mathiasmayabi@students.uonbi.ac.ke>
2024-02-29 16:41:05 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,51 @@
+//void main() {
+  // //int
+  // int num1 = 10
+  // //double
+  // double num2 = 5.5
+  // print (num1)
+  // print (num2)
+  // example of a string data type
+  // String school = "Power Learn Project";
+  // String address = "Mombasa";
+  // print (Hey there I am kamash a student at $school and I come from $address)
+  // example of boolean as a datatype
+  // bool isgraduated = true;
+  //print ("graduation status:$isgraduated");
+  //lists
+  //lists
+// writing function outside main function.
+///void printName(){
+ // print("My name John James");
+//}
+// this is our main function.
+//void main(){
+ // printName();
+//}
+//void add(int num1, int num2){
+  //int sum = num1 + num2;
+  // print("The sum is $sum");
+//}
+
+//void main(){
+ // add(10, 20);
+//}
+//void main(){
+  //stringLength(){
+   // List name = ['kamash','monicah','bertha'];
+    //String name ="Monicah";
+    //return name.length;
+  //  return name[2];
+ // }
+ // print (stringLength());
+//}
+void main(){
+  int addTwo(){
+    print("enter the first no:");
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("enter the second no:");
+    int num2 = int.parse(stdin.readLineSync()!);
+      }
+  }
+  print (stringLength());
+}
